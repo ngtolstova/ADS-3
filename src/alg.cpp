0436757e26201bfc8769a9011fc25b9a583b1623
ngtolstova <124882480+ngtolstova@users.noreply.github.com>
2023-03-12 16:14:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,37 +1,31 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int k = size / 2 - 1, start, stop, flag = 1, count = 0;
-  while (arr[k] != value) {
-    if (arr[k] > value) {
-      if (k == 0) {
-        flag = 0;
-        break;
-       }
-      stop = k;
-      k = k / 2;
-      size = stop + 1;
-    }
-    else if (arr[k + 1] <= value) {
-      if (k + 1 >= size) {
-        flag = 0;
-        break;
+  int start = 0, 
+  int stop = size - 1;
+  int ksred, i, count = 0;
+  while (start <= stop) {
+    ksred = (start + stop) / 2;
+    if (arr[ksred] == value) {
+      count++;
+      i = ksred - 1;
+      while (i >= 0 && arr[i] == value) {
+        count++;
+        i--;
       }
-      start = k + 1;
-      stop = size - 1;
-      size = stop - start + 1;
-      arr = arr + start;
-      k = (stop - start) / 2;
+      i = ksred + 1;
+      while (i < size && arr[i] == value) {
+        count++;
+        i++;
+      }
+        break;
     }
-  }
-  if (flag == 0) return 0;
-  count = 1; int tmp_k = k;
-  while (arr[++k] == value && k<size) {
-  count++;
-  }
-  k = tmp_k;
-  while (arr[--k] == value && k >= 0) {
-    count++;
-  }
-  return count;
+    else if (arr[ksred] < value) {
+      start = ksred + 1;
+    }
+    else {
+      stop = ksred - 1;
+    }
+ }
+ return count;
 }
